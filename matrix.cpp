0e3be33d3aa6ff38091fcadcf5f5b1874c61ae21
Song-Li <lisong1013@gmail.com>
2018-10-30 20:26:26 +0000

    pass array to webassembly

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,13 @@
-char* matrix(char* ori, int bufSize) {
-  for (int i = 0;i < bufSize;++ i)
-    ori[i] <<= 1;
-  return ori;
+const int SIZE = 100;
+int buffer[SIZE];
+int* matrix() {
+  for (int i = 0;i < SIZE;++ i)
+    buffer[i] <<= 1;
+  return buffer;
+}
+
+int getBuffer() {
+  return (int)&buffer[0];
 }
 
 int main() {
--- matrix.js
+++ matrix.js
@@ -1,37 +1,33 @@
 function loadWebAssembly(filename, imports) {
-  return fetch(filename)
+  imports = imports || {};
+  imports.env = imports.env || {};
+  imports.env.memoryBase = imports.env.memoryBase || 0;
+  imports.env.tableBase = imports.env.tableBase || 0;
+  if (!imports.env.memory) {
+    imports.env.memory = new WebAssembly.Memory({ initial: 256 });
+  }
+  if (!imports.env.table) {
+    imports.env.table = new WebAssembly.Table({ initial: 0, 
+      element: 'anyfunc' });
+  }
+  return [fetch(filename)
     .then(response => response.arrayBuffer())
     .then(buffer => WebAssembly.compile(buffer))
     .then(module => {
-      imports = imports || {};
-      imports.env = imports.env || {};
-      imports.env.memoryBase = imports.env.memoryBase || 0;
-      imports.env.tableBase = imports.env.tableBase || 0;
-      if (!imports.env.memory) {
-        imports.env.memory = new WebAssembly.Memory({ initial: 256 });
-      }
-      if (!imports.env.table) {
-        imports.env.table = new WebAssembly.Table({ initial: 0, 
-          element: 'anyfunc' });
-      }
       return new WebAssembly.Instance(module, imports);
-    });
+    }), imports];
 }
 
-// Main part of this example, loads the module and uses it.
-var loadedWasm = loadWebAssembly('matrix.wasm');
-loadedWasm.then(instance => {
-  console.log(instance.exports);
-  var arrayDataToPass = [1,2,3,4,5,6];
-  const typedArray = new Uint8Array(arrayDataToPass);
-
-  // Allocate some space in the heap for the data (making sure to use the appropriate memory size of the elements)
-  buffer = instance.Module._malloc(typedArray.length * typedArray.BYTES_PER_ELEMENT)
-
-  // Assign the data to the heap - Keep in mind bytes per element
-  Module.HEAPF32.set(typedArray, buffer >> 2)
-
-  // Finally, call the function with "number" parameter type for the array (the pointer), and an extra length parameter
-  result = Module.ccall("addNums", null, ["number", "number"], [buffer, arrayDataToPass.length])
-}
-);
+var [wasmInstance, imports] = loadWebAssembly("./matrix.wasm");
+wasmInstance.then(instance => {
+  var offset = instance.exports.__Z9getBufferv();
+  // create a view on the memory that points to this array
+  var linearMemory = new Uint32Array(imports.env.memory.buffer, offset, 100);
+  // populate with some data
+  for (var i = 0; i < linearMemory.length; i++) {
+    linearMemory[i] = i;
+  }
+  // mutate the array within the WebAssembly module
+  instance.exports.__Z6matrixv();
+  console.log(linearMemory);
+})
