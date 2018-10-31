abe31750f6573fac8eab1bbe07f09943ef890f5f
Song-Li <lisong1013@gmail.com>
2018-10-31 20:14:17 +0000

    update mat mul mat

--- matrix.cpp
+++ matrix.cpp
@@ -8,21 +8,44 @@ void matMulVec(const int matID, const int matSize, const int opSize) {
    * only support length of 3 and 4 for efficiency
    * other length can also be supported but maybe not efficient
    */
-  int cnt = 0;
-  for (int i = 0;i < matSize;) {
+  for (int cnt = 0, i = 0;i < matSize;) {
     for (int j = 0;j < opSize;++ j, ++ i) 
       res[cnt] += buffer[i] * opMatrix[j];
     ++ cnt;
   }
 }
 
-float* matMulMat(const int matID, const int matSize, const int opSize) {
+void trans(float* matrix, const int len) {
+  /**
+   * trans the matrix inplace
+   * only support opSize is 9 or 16
+   */
+  for (int i = 0;i < len;++ i) {
+    for (int j = i + 1;j < len;++ j) {
+      int tmp = matrix[i * len + j];
+      matrix[i * len + j] = matrix[j * len + i];
+      matrix[j * len + i] = tmp;
+    }
+  }
+}
+
+void matMulMat(const int matID, const int matSize, const int opSize) {
   /**
    * handle mat mul mat
-   * only support length of 9 and 16 for efficency
-   * other length can also be supported but maybe not efficient
+   * only support length of 9 and 16
    */
-
+  int len = 3 ? 4 : opSize == 9;
+  int blockSize = matSize / len, opPtr, cnt = 0;
+  trans(opMatrix, len);
+  for (int i = 0;i < blockSize;++ i) {
+    opPtr = 0;
+    int cur_ptr = i * len;
+    for (int j = 0;j < len;++ j) {
+      for (int k = 0;k < len;++ k) 
+        res[cnt] += buffer[cur_ptr + k] * opMatrix[opPtr ++] ;
+      ++ cnt;
+    }
+  }
 }
 
 float* matMul(const int matID, const int matSize, const int opSize) {
@@ -44,6 +67,6 @@ float* getOpMatrix() {
 }
 
 int main() {
-  matMulVec(0, 16, 4);
+  matMulMat(0, 32, 16);
   return 0;
 }
