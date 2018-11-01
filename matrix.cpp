964be75de6c803133a97255f37d484c2aa897ec4
Song-Li <lisong1013@gmail.com>
2018-11-01 19:36:23 +0000

    almost finished

--- matrix.cpp
+++ matrix.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 const int SIZE = 65535;
 float buffer[SIZE], opMatrix[16], res[SIZE];
+int offset[SIZE], curSIZE = 0;
 
 int matMulVec(const int matID, const int matSize, const int opSize) {
   /**
@@ -8,10 +9,11 @@ int matMulVec(const int matID, const int matSize, const int opSize) {
    * only support length of 3 and 4 for efficiency
    * other length can also be supported but maybe not efficient
    */
+  float* cur_buffer = buffer + offset[matID];
   int cnt = 0;
   for (int i = 0;i < matSize;) {
     for (int j = 0;j < opSize;++ j, ++ i) 
-      res[cnt] += buffer[i] * opMatrix[j];
+      res[cnt] += cur_buffer[i] * opMatrix[j];
     ++ cnt;
   }
   return cnt;
@@ -36,6 +38,7 @@ int matMulMat(const int matID, const int matSize, const int opSize) {
    * handle mat mul mat
    * only support length of 9 and 16
    */
+  float* cur_buffer = buffer + offset[matID];
   int len = opSize == 9 ? 3 : 4;
   int blockSize = matSize / len, opPtr, cnt = 0;
   trans(opMatrix, len);
@@ -45,7 +48,7 @@ int matMulMat(const int matID, const int matSize, const int opSize) {
     for (int j = 0;j < len;++ j) {
       res[cnt] = 0;
       for (int k = 0;k < len;++ k) 
-        res[cnt] += buffer[cur_ptr + k] * opMatrix[opPtr ++] ;
+        res[cnt] += cur_buffer[cur_ptr + k] * opMatrix[opPtr ++] ;
       ++ cnt;
     }
   }
@@ -61,6 +64,12 @@ int matMul(const int matID, const int matSize, const int opSize) {
    */
   if (9 == opSize || 16 == opSize) return matMulMat(matID, matSize, opSize);
   return matMulVec(matID, matSize, opSize);
+}
+
+float* loadMatrix(const int matID, const int length) {
+  offset[matID] = curSIZE;
+  curSIZE += length;
+  return (&buffer[offset[matID]]);
 }
 
 float* getBufferPtr() {
--- matrix.js
+++ matrix.js
@@ -1,47 +1,88 @@
-function loadWebAssembly(filename, imports) {
-  imports = imports || {};
-  imports.env = imports.env || {};
-  imports.env.memoryBase = imports.env.memoryBase || 0;
-  imports.env.tableBase = imports.env.tableBase || 0;
-  if (!imports.env.memory) {
-    // the number of pages, 64K per page
-    imports.env.memory = new WebAssembly.Memory({ initial: 256 });
+class MatrixCalculator {
+  /**
+   * @constructs MatrixCalculator()
+   * @param 
+   */
+
+  constructor(filename, imports) {
+    // the bufferMap is used to store the matID and bufferMap
+    this.bufferMap = {};
+
+    imports = imports || {};
+    imports.env = imports.env || {};
+    imports.env.memoryBase = imports.env.memoryBase || 0;
+    imports.env.tableBase = imports.env.tableBase || 0;
+    if (!imports.env.memory) {
+      // the number of pages, 64K per page
+      imports.env.memory = new WebAssembly.Memory({ initial: 256 });
+    }
+    if (!imports.env.table) {
+      imports.env.table = new WebAssembly.Table({ initial: 0, 
+        element: 'anyfunc' });
+    }
+
+    this.imports = imports;
+    this.SIZE = 65535;
+    this.instance = null;
+    var wasmInstance = this.loadWebAssembly(filename, imports);
+    wasmInstance.then(instance => {this.instance = instance;
+      this.opMatrixPtr = instance.exports.__Z14getOpMatrixPtrv();
+      this.opMatrix = new Float32Array(this.imports.env.memory.buffer, this.opMatrixPtr, 16);
+      this.resPtr = instance.exports.__Z9getResPtrv();
+      this.res = new Float32Array(this.imports.env.memory.buffer, this.resPtr, this.SIZE);
+      runCal()});
   }
-  if (!imports.env.table) {
-    imports.env.table = new WebAssembly.Table({ initial: 0, 
-      element: 'anyfunc' });
+
+  loadWebAssembly(filename, imports) {
+    return fetch(filename)
+      .then(response => response.arrayBuffer())
+      .then(buffer => WebAssembly.compile(buffer))
+      .then(module => {
+        return new WebAssembly.Instance(module, imports);
+      });
   }
-  return [fetch(filename)
-    .then(response => response.arrayBuffer())
-    .then(buffer => WebAssembly.compile(buffer))
-    .then(module => {
-      return new WebAssembly.Instance(module, imports);
-    }), imports];
+
+  loadMatrix(matID, matVal) {
+    var matLength = matVal.length;
+    var bufferPtr = this.instance.exports.__Z10loadMatrixii(matID, matLength);
+    this.bufferMap[matID] = new Float32Array(this.imports.env.memory.buffer, bufferPtr, matLength);
+    this.bufferMap[matID].set(matVal);
+  }
+
+
+  doMatMul(matID, opMatrix) {
+    this.opMatrix.set(opMatrix); 
+    var resLength = this.instance.exports.__Z6matMuliii(matID, this.bufferMap[matID].length, opMatrix.length);
+    return resLength;
+  }
+
 }
 
-var [wasmInstance, imports] = loadWebAssembly("./matrix.wasm");
-wasmInstance.then(instance => {
-  var bufferPtr = instance.exports.__Z12getBufferPtrv();
-  var opMatrixPtr = instance.exports.__Z14getOpMatrixPtrv();
-  var resPtr = instance.exports.__Z9getResPtrv()
-  console.log(instance.exports);
-  console.log(bufferPtr);
-  console.log(opMatrixPtr);
-  console.log(resPtr);
-  var bufferLength = 1024;
-  var buffer = new Float32Array(imports.env.memory.buffer, bufferPtr, bufferLength);
-  var opMatrix = new Float32Array(imports.env.memory.buffer, opMatrixPtr, 16);
-  for (var i = 0; i < buffer.length; i++) {
-    buffer[i] = i;
-    opMatrix[i] = i;
-  }
+var matrixCalculator = new MatrixCalculator('./matrix.wasm'); 
+
+function runCal() {
+  var instance = matrixCalculator.instance;
+  var imports = matrixCalculator.imports;
   var t0 = performance.now();
-  for (var i = 0;i < 100;++ i)
-    var resLength = instance.exports.__Z6matMuliii(0, bufferLength, 16);
+  var mat1 = [];
+  for (var i = 0;i < 40000;++ i) mat1.push(i);
+  var mat2 = [];
+  for (var i = 0;i < 30;++ i) mat2.push(i);
+
+  matrixCalculator.loadMatrix(0, mat1);
+  matrixCalculator.loadMatrix(1, mat2);
+
+  var opMatrix = [];
+  for (var i = 0;i < 16;++ i) opMatrix.push(i);
+
+  var resLength = matrixCalculator.doMatMul(0, opMatrix);
+
   var t1 = performance.now();
-  var res = new Float32Array(imports.env.memory.buffer, resPtr, resLength);
-  console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-  console.log('buffer', buffer);
-  console.log('opMatrix', opMatrix);
-  console.log('res', res);
-});
+  console.log('time in ms: ', t1 - t0);
+  console.log('res', matrixCalculator.res);
+
+  var opMatrix = [];
+  for (var i = 0;i < 9;++ i) opMatrix.push(i);
+  var resLength = matrixCalculator.doMatMul(1, opMatrix);
+  console.log('res', matrixCalculator.res);
+}
